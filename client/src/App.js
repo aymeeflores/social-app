4a17b4738241c60cbb68c3814cc7a4da65e585f8
aymeeflores <aymee.flores@gmail.com>
2022-06-22 22:53:50 -0400

    profile details component

--- client/src/App.js
+++ client/src/App.js
@@ -3,11 +3,12 @@ import React from "react";
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 import Login from "./pages/Login/Login.js";
 import Feed from "./components/Feed/Feed";
+import Profilecard from "./components/Profilecard/Profilecard";
 
 function App() {
 	return (
 		<div>
-			<Feed />
+			<Profilecard />
 		</div>
 	);
 }
