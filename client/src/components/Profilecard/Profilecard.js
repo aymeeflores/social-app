76db0e04c2b3024945ad98292e655c1b69c39605
aymeeflores <aymee.flores@gmail.com>
2022-06-23 21:57:48 -0400

    profilecard responsiveness

--- client/src/components/Profilecard/Profilecard.js
+++ client/src/components/Profilecard/Profilecard.js
@@ -1,17 +1,17 @@
 import React from "react";
+import Profilepic from "../../assets/Profilepic.jpg";
 
 function Profilecard() {
 	return (
 		<>
 			<div className="bg-white shadow overflow-hidden sm:rounded-lg">
-				<div className="px-4 py-5 sm:px-6">
+				{/* <div className="px-4 py-5 sm:px-6">
 					<h3 className="text-lg leading-6 font-medium text-gray-900">Applicant Information</h3>
 					<p className="mt-1 max-w-2xl text-sm text-gray-500">Personal details and application.</p>
-				</div>
-				<div className="flex md: flex-wrap">
-					<div className="h-full">
-						<img src="#" alt="profile pic " />
-					</div>
+				</div> */}
+				<div className="flex md: flex-wrap justify-center">
+					<img src={Profilepic} className="max-w-sm  rounded-2xl m-4  " alt="profile pic " />
+
 					<div>
 						<div className="border-t border-gray-200">
 							<dl>
